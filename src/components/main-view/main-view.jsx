8f692b5d59db4894a27777b5d52a2507f6bd7736
deliretik <deliretik@gmail.com>
2021-12-15 13:51:57 +0100

    remove this function from movie card

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
@@ -43,13 +44,13 @@ export class MainView extends React.Component {
     }
     render() {
         const { movies, selectedMovie } = this.state;
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;//The list is empty!</div>;
         return (
             <div className="main-view">
               {selectedMovie
                 ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
                 : movies.map(movie => (
-                  <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+                  <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
                 ))
               }
             </div>
