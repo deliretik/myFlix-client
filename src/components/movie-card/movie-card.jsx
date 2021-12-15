4394b7ba1d44b745f4f5b3beee2b9847e4913bd8
deliretik <deliretik@gmail.com>
2021-12-15 21:31:42 +0100

    updated moviecard proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,14 +11,14 @@ export class MovieCard extends React.Component {
     }
 }
 
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     Title: PropTypes.string.isRequired,
+//     Description: PropTypes.string.isRequired,
+//     ImagePath: PropTypes.string.isRequired
+//   }).isRequired,
+//   onMovieClick: PropTypes.func.isRequired
+// };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
