4d111e932963bf8dac22438702746a93a88cf593
deliretik <deliretik@gmail.com>
2021-12-23 20:52:23 +0100

    update

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CardColumns from 'react-bootstrap/CardColumns';
-import Image from "react-bootstrap/Image";
+import Image from 'react-bootstrap/Image';
 
 
 export class MovieCard extends React.Component {
