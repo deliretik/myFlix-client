7c129e241719ab331d5df61c79d520633ca00198
deliretik <deliretik@gmail.com>
2021-12-23 20:35:47 +0100

    updated login and movie card

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 
@@ -10,21 +11,34 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+    axios.post('https://flexmyflix.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response =>{
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('no such user')
+    });
+  };
+  
+    //console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-     props.onLoggedIn(username);
+     
   };
 
   return (
     <Form>
       <Form.Group controlId="formUsername">
       <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" value={username} placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
       </Form.Group>
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <Form.Control type="password" value={password} placeholder="Password" onChange={e => setPassword(e.target.value)} />
       </Form.Group>
       <Button type="submit" onClick={handleSubmit}>Sign in</Button>
       <Button type="submit">Register</Button>
